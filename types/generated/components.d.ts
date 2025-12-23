278f9821618bbfe03aba7f85ae3927db445dae6e
vishnu <vishnu@brandstory.in>
2025-12-23 12:36:43 +0530

    Section 5 create a component repeat section

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,20 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SectionEmpowerItem extends Struct.ComponentSchema {
+  collectionName: 'components_section_empower_items';
+  info: {
+    displayName: 'empower-item';
+  };
+  attributes: {
+    description: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'section.empower-item': SectionEmpowerItem;
+    }
+  }
+}
